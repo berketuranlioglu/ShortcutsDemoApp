386b7dcce16d0ba1ef17a734948dac0b35b66ee9
Berke Turanlıoğlu <berke.turanlioglu@gmail.com>
2024-01-12 22:11:10 +0300

    Initial Commit

---
+++ ShortcutsDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShortcutsDemoApp
+//
+//  Created by Berke Turanlioglu on 12.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShortcutsDemoApp/ShortcutsDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutsDemoAppApp.swift
+//  ShortcutsDemoApp
+//
+//  Created by Berke Turanlioglu on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutsDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
